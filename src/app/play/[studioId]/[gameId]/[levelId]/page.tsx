dda2abfd691b247bad85546b876805ca9ca727c1
Merlin <merlin.cornu@gmail.com>
2026-03-27 00:25:10 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/play/[studioId]/[gameId]/[levelId]/page.tsx
+++ src/app/play/[studioId]/[gameId]/[levelId]/page.tsx
@@ -1,15 +1,2 @@
-
 "use client";
-import { useEffect } from 'react';
-import { useRouter, useParams } from 'next/navigation';
-
-export default function RedirectToSession() {
-  const router = useRouter();
-  const { levelId } = useParams();
-  
-  useEffect(() => {
-    if (levelId) router.replace(`/session/${levelId}`);
-  }, [router, levelId]);
-
-  return null;
-}
+export default function RemovedConflict() { return null; }
